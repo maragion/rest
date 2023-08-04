90edc56644f3c11f3bc10e59b7d84b11770a0def
Aleksei Taraskin <bakamoena@gmail.com>
2023-08-04 15:15:04 +0700

    get by id.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const data = require("./db.json");
-
 const cors = require("cors");
+
 const app = express();
 const port = 5000;
 
@@ -9,16 +9,48 @@ app.use(cors());
 app.use(express.json());
 
 
+//get
+
 app.get("/users", (req, res) => {
-    // res.status(200).send("Hello")
-    res.json(data)
+    res.send(data.users);
 })
 
-app.post("/users", (req, res)  => {
-        data.users.push(req.body)
-        res.json(data)
-        console.log(data)
+app.get("/requests", (req, res) => {
+    res.send(data.requests)
+})
 
+
+// Get by id
+
+app.get("/users:id", (req, res) => {
+    let id = req.params.id;
+
+    if (data.users[id]) {
+        res.send(data.users[id]);
+    } else res.status(404).send("Error: 404. User Not Found");
+
+})
+
+app.get("/requests:id", (req, res) => {
+    let id = req.params.id;
+    if (data.requests[id]) {
+        res.send(data.requests[id]);
+    } else
+        res.status(404).send("Error: 404. Request no found");
+
+})
+
+
+// Post
+
+app.post("/users", (req, res) => {
+    data.users.push(req.body);
+    res.send(data.users);
+})
+
+app.post("/requests", (req, res) => {
+    data.requests.push(req.body);
+    res.send(data.requests);
 })
 
 
